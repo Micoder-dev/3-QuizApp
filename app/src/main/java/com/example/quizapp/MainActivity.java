7fd504e9796308edc2dfa73eb409b230413b20e0
Micoder-dev <micoder.com@gmail.com>
2022-07-17 00:38:50 +0530

    btn's onClickListeners Added with blank

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnWrong = findViewById(R.id.btnWrong);
+        Button btnTrue = findViewById(R.id.btnTrue);
+
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnWrong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
